b722bd892cb3609bf8885e5c8f835a52c787b7e1
amunden04 <amunden04@gmail.com>
2019-11-22 03:36:22 -0700

    updates to search course

---
+++ searchCourse.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/main-style.css">
+	</head>	
+
+<body>
+<br><center><h3>Results</h3></center><br>
+
+<table id = "tables">
+	<tr>
+        <th>Course ID</th>
+        <th>Section</th>
+        <th>Days</th>
+		<th>Times</th>
+		<th>Classroom</th>
+	</tr>
+
+<?php
+	include 'connectdb.php';
+	$conn = connect_sql();
+
+	if (isset($_POST['course_search_submit']))
+	{
+	$section = $classroom = "";
+	$course_id = 0;
+
+	$course_id = mysqli_real_escape_string($conn, $_POST['courseID']);
+	$section = mysqli_real_escape_string($conn, $_POST['section']);
+	$classroom = mysqli_real_escape_string($conn, $_POST['classroom']);
+
+	$courseIDtoEdit = [];
+	$i = 0;
+
+	$sql = "SELECT * FROM course WHERE((courseID = '$course_id') OR (section = '$section')
+			OR (classroom = '$classroom'))";	
+			
+	$result = mysqli_query($conn, $sql);
+
+	if (! $result) 
+	die ('could not get data: ' . mysqli_error($conn));
+
+	if( mysqli_num_rows($result) > 0){
+		while($row = mysqli_fetch_assoc($result))
+		{
+			$i = 0;
+			$courseIDtoEdit[$i] = ($row['courseID']);
+			echo "<form method='post' action='UpdateCustomer.php'>";
+			echo "<input type='hidden' name='courseID' value= $courseIDtoEdit[$i]>";
+            echo "<tr>"."<td>" . $row["courseID"] . "</td>" .
+            "<td>" . $row["section"] . "</td>" .
+			"<td>" . $row["daysTaught"] . "</td>" .
+			"<td>" . $row["timeTaught"] . "</td>" .
+			"<td>" . $row["classroom"] . "</td>" . "</form>" . "</td>" .
+			"</tr>";
+			$i = ($i+1);
+			
+		}
+	 } else {
+			echo "0 Results";
+		}
+	}
+		mysqli_close($conn);
+
+?>
+</table>
+
+<br><br>
+<input type="button" value="Return Home" onclick="window.location.href='menu.html'" />
+
+</html>
