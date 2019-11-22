bf3b8b15a51b185f70b722061a26b6b9944635a2
amunden04 <amunden04@gmail.com>
2019-11-21 23:34:53 -0700

    completed file to create the database

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,17 +1,45 @@
 create table student
 (
-    
-
+    StudentID int not null auto_increment,
+    Usernames varchar(40),
+    Passwords varchar(30),
+    fName varchar(20),
+    lName varchar(20),
+    streetName varchar(30)
+    city varchar(20),
+    province varchar(20),
+    postCode char(7),
+    Phone char(14),
+    DOB char(10),
+    gender char(1),
+    enrolledCourses varchar(25),
+    primary key (StudentID)
 );
 
 create table faculty
 (
-
-
+    StaffID int not null auto_increment, 
+    Usernames varchar(40),
+    Passwords varchar(30),
+    fName varchar(20),
+    lName varchar(20),
+    streetName varchar(30),
+    city varchar(20),
+    province varchar(20),
+    postCode char(7),
+    Phone char(14),
+    DOB char(10),
+    gender char(1),
+    coursesTaught varchar(25),
+    primary key (StaffID)
 );
 
 create table course 
 (
-
-
+    courseID int not null auto_increment,
+    section char(1),
+    daysTaught char(3),
+    timeTaught varchar(11),
+    classroom varchar(8),
+    primary key (courseID)
 );
