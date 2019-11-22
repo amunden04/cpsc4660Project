9ff42c99b2ea8e3808dbb3893d62ce666759c651
muna2720 <amanda.munden@uleth.ca>
2019-11-22 16:19:33 -0700

    updating add course to reflect the changes in the database

--- addCourse.php
+++ addCourse.php
@@ -4,18 +4,20 @@
 
     if (isset($_POST['course_submit']))
     {
-    $section = $days = $times = $classroom = "";
+    $courseNum = $courseName = $section = $days = $times = $classroom = "";
 	
-    $section = mysqli_real_escape_string($conn, $_POST['section']);
+	$courseNum = mysqli_real_escape_string($conn, $_POST['courseNum']);
+	$courseName = mysqli_real_escape_string($conn, $_POST['courseName']);
+	$section = mysqli_real_escape_string($conn, $_POST['section']);
     $days = mysqli_real_escape_string($conn, $_POST['daysTaught']);
 	$times = mysqli_real_escape_string($conn, $_POST['timeTaught']);
 	$classroom = mysqli_real_escape_string($conn, $_POST['classroom']);
 	
-    $sql = "INSERT INTO course VALUES(NULL, '$section', '$days', '$times', '$classroom')";
+    $sql = "INSERT INTO course VALUES(NULL, '$courseNum', '$courseName', '$section', '$days', '$times', '$classroom')";
 
     $retval = mysqli_query($conn, $sql);
     
-	$url = "http://localhost/cpsc4660Project/AddCourse.html";
+	$url = "http://localhost/cpsc4660Project/menu.html";
 	if($retval){
 		header("Location: $url");
 	exit;
