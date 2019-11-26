0e9f50e8a3be6d15538f0e845dbc48f8256ae668
muna2720 <amanda.munden@uleth.ca>
2019-11-25 23:35:49 -0700

    adding the completed SKW add files

--- addCourseSKW.php
+++ addCourseSKW.php
@@ -1,17 +1,25 @@
 <?php
-    include 'connectdb.php';
+	include 'connectdb.php';
+	include 'SKWFunction.php';
     $conn = connect_sql();
 
     if (isset($_POST['course_submit']))
     {
+	$temp1 = $temp2 = $temp3 = $temp4 = $temp5 = $temp6 = "";
     $courseNum = $courseName = $section = $days = $times = $classroom = "";
-	
-	$courseNum = mysqli_real_escape_string($conn, $_POST['courseNum']);
-	$courseName = mysqli_real_escape_string($conn, $_POST['courseName']);
-	$section = mysqli_real_escape_string($conn, $_POST['section']);
-    $days = mysqli_real_escape_string($conn, $_POST['daysTaught']);
-	$times = mysqli_real_escape_string($conn, $_POST['timeTaught']);
-	$classroom = mysqli_real_escape_string($conn, $_POST['classroom']);
+	$temp1 = $_POST['courseNum'];
+	$temp2 = $_POST['courseName'];
+	$temp3 = $_POST['section'];
+	$temp4 = $_POST['daysTaught'];
+	$temp5 = $_POST['timeTaught'];
+	$temp6 = $_POST['classroom'];
+
+	$courseNum = skw($temp1);
+	$courseName = skw($temp2);
+	$section = skw($temp3);
+    $days = skw($temp4);
+	$times = skw($temp5);
+	$classroom = skw($temp6);
 	
     $sql = "INSERT INTO course VALUES(NULL, '$courseNum', '$courseName', '$section', '$days', '$times', '$classroom')";
 
--- addStudentSKW.php
+++ addStudentSKW.php
@@ -1,23 +1,40 @@
 <?php
-    include 'connectdb.php';
+	include 'connectdb.php';
+	include 'SKWFunction.php';
     $conn = connect_sql();
 
     if (isset($_POST['student_submit']))
     {
+	$temp1 = $temp2 = $temp3 = $temp4 = $temp5 = $temp6 = "";
+	$temp7 = $temp8 = $temp9 = $temp10 = "";
+	$temp11 = '';
     $username = $password = $first_name = $last_name = $address = $city = $province = $phone = $postCode = $dob = "";
 	$gender = '';
+
+	$temp1 = $_POST['username'];
+	$temp2 = $_POST['password'];
+	$temp3 = $_POST['fName'];
+	$temp4 = $_POST['lName'];
+	$temp5 = $_POST['streetName'];
+	$temp6 = $_POST['city'];
+	$temp7 = $_POST['province'];
+	$temp8 = $_POST['postCode'];
+	$temp9 = $_POST['phone'];
+	$temp10 = $_POST['dob'];
+	$temp11 = $_POST['gender'];
+	
     
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
-	$first_name = mysqli_real_escape_string($conn, $_POST['fName']);
-	$last_name = mysqli_real_escape_string($conn, $_POST['lName']);
-	$address = mysqli_real_escape_string($conn, $_POST['streetName']);
-	$city = mysqli_real_escape_string($conn, $_POST['city']);
-	$province = mysqli_real_escape_string($conn, $_POST['province']);
-	$postCode = mysqli_real_escape_string($conn, $_POST['postCode']);
-	$phone = mysqli_real_escape_string($conn, $_POST['phone']);
-	$dob = mysqli_real_escape_string($conn, $_POST['dob']);
-	$gender = mysqli_real_escape_string($conn, $_POST['gender']);
+    $username = skw($temp1);
+    $password = skw($temp2);
+	$first_name = skw($temp3);
+	$last_name = skw($temp4);
+	$address = skw($temp5);
+	$city = skw($temp6);
+	$province = skw($temp7);
+	$postCode = skw($temp8);
+	$phone = skw($temp9);
+	$dob = skw($temp10);
+	$gender = skw($temp11);
 
     $sql = "INSERT INTO student VALUES(NULL, '$username', '$password', '$first_name', '$last_name', '$address', '$city', '$province', '$postCode', '$phone', '$dob', '$gender', NULL)";
 
