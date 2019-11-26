295a5978a13d9debf1c83a53bd97ad4eb53aed4c
muna2720 <amanda.munden@uleth.ca>
2019-11-25 19:04:38 -0700

    changes to test file

--- test.php
+++ test.php
@@ -21,37 +21,45 @@
 <?php
 	include 'connectdb.php';
 	$conn = connect_sql();
+
+	function mres($value){
+		$search = array("\\", "\x00", "\n", "\r", "'", '"', "\x1a");
+		$replace = array("\\\\", "\\0", "\\n", "\\r", "\\'", '\\"', "\\z");
+		return str_replace($search, $replace, $value);
+	}
 	
 	if (isset($_POST['course_search_submit']))
 	{
 		$temp = $courseName = "";
 		$temp = $_POST['courseName'];
-		$courseName = addcslashes($temp, ";',");
-
-	$sql = "SELECT * FROM course WHERE courseName LIKE '%".$courseName."%'";
-	$result = mysqli_query($conn, $sql);
-
-	if (! $result) 
-	die ('could not get data: ' . mysqli_error($conn));
-
-	if( mysqli_num_rows($result) > 0){
-		while($row = mysqli_fetch_assoc($result))
-		{
-            echo "<tr>"."<td>" . $row["courseID"] . "</td>" .
-			"<td>" . $row["courseNum"] . "</td>" .
-			"<td>" . $row["courseName"] . "</td>" .
-			"<td>" . $row["section"] . "</td>" .
-			"<td>" . $row["daysTaught"] . "</td>" .
-			"<td>" . $row["timeTaught"] . "</td>" .
-			"<td>" . $row["classroom"] . "</td>" . "</form>" . "</td>" .
-			"</tr>";
-			
+		$courseName = mres($temp);
+		echo gettype($courseName);
+		echo " ";
+		echo $courseName;
+		$sql = "SELECT * FROM course WHERE courseName LIKE '%".$courseName."%'";
+		$result = mysqli_query($conn, $sql);
+	
+		if (! $result) 
+		die ('could not get data: ' . mysqli_error($conn));
+	
+		if( mysqli_num_rows($result) > 0){
+			while($row = mysqli_fetch_assoc($result))
+			{
+				echo "<tr>"."<td>" . $row["courseID"] . "</td>" .
+				"<td>" . $row["courseNum"] . "</td>" .
+				"<td>" . $row["courseName"] . "</td>" .
+				"<td>" . $row["section"] . "</td>" .
+				"<td>" . $row["daysTaught"] . "</td>" .
+				"<td>" . $row["timeTaught"] . "</td>" .
+				"<td>" . $row["classroom"] . "</td>" . "</form>" . "</td>" .
+				"</tr>";
+				
+			}
+		 } else {
+				echo "0 Results";
+			}
 		}
-	 } else {
-			echo "0 Results";
-		}
-	}
-		mysqli_close($conn);
+			mysqli_close($conn);
 	
 ?>
 
